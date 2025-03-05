697eafa9013603f25633b7c2199c6311ca638ea5
SuwitChanthasen <166077564+BossDever@users.noreply.github.com>
2025-03-06 02:53:58 +0700

    Auto-commit at 2025-03-06 02:53:58

--- ai-server/app.py
+++ ai-server/app.py
@@ -2,3 +2,4 @@ import tensorflow as tf
 print("TensorFlow built with CUDA:", tf.test.is_built_with_cuda())
 print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
 print(tf.config.list_physical_devices('GPU'))
+print(tf.sysconfig.get_build_info())  # เช็คว่าต่อกับ CUDA หรือไม่
