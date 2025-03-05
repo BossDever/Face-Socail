64e50e2dc5b0e010db68b84f955495a6072876e1
SuwitChanthasen <166077564+BossDever@users.noreply.github.com>
2025-03-06 02:36:18 +0700

    Auto-commit at 2025-03-06 02:36:17

--- ai-server/app.py
+++ ai-server/app.py
@@ -1,4 +1,3 @@
 import tensorflow as tf
-print(tf.test.is_built_with_cuda())  # ตรวจสอบว่า TensorFlow ถูกคอมไพล์พร้อม CUDA หรือไม่
-print(tf.test.is_gpu_available())    # ตรวจสอบว่าพบ GPU หรือไม่ (เก่า)
-print(tf.config.list_physical_devices('GPU'))  # ตรวจสอบ GPU ที่ใช้งานได้ (ใหม่)
+print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
+print(tf.config.list_physical_devices('GPU'))
