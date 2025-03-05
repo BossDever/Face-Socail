a474b2e514995b250370d69818318b5c0c35e274
SuwitChanthasen <166077564+BossDever@users.noreply.github.com>
2025-03-06 01:32:02 +0700

    Auto-commit at 2025-03-06 01:32:02

--- ai-server/png.py
+++ ai-server/png.py
@@ -1,7 +1,9 @@
 import base64
+import os
 
 # แปลงรูปภาพเป็น base64
-with open("C:\Users\suwit\Downloads\95613_0.jpg", "rb") as image_file:
+image_path = os.path.join(os.path.dirname(__file__), "95613_0.jpg")
+with open(image_path, "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
 
 # เพิ่ม prefix สำหรับ base64 image
