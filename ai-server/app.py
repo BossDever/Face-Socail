f0e7a6957ce72ec976b7ba7fa787b844ad524355
SuwitChanthasen <166077564+BossDever@users.noreply.github.com>
2025-03-06 02:53:25 +0700

    Auto-commit at 2025-03-06 02:53:25

--- ai-server/app.py
+++ ai-server/app.py
@@ -1,3 +1,4 @@
 import tensorflow as tf
+print("TensorFlow built with CUDA:", tf.test.is_built_with_cuda())
 print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
 print(tf.config.list_physical_devices('GPU'))
