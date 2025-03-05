d770329e6ec55dd23d61f1483e450cae96abfa24
SuwitChanthasen <166077564+BossDever@users.noreply.github.com>
2025-03-06 00:25:58 +0700

    Auto-commit at 2025-03-06 00:25:57

--- schedule-commit.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d D:\FinalProject\Face-Socail
-bash auto-commit.sh "Auto-commit at %DATE% %TIME%"
--- start-auto-commit.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File "D:\FinalProject\Face-Socail\auto-commit-loop.ps1"
-pause
